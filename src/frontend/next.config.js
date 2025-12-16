c387c36531170ea431689ee3b3f56479251bb37a
Ole Martin Borge <ole.borge@twoday.com>
2025-12-16 18:15:19 +0100

    linting

--- src/frontend/next.config.js
+++ src/frontend/next.config.js
@@ -1,61 +1,56 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  logging: {
-    fetches: {
-      fullUrl: true,
-    },
+  images: {
+    // Configure remote image patterns for Next.js Image component
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '8000',
+        pathname: '/media/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.cdninstagram.com', // Match all Instagram CDN hostnames
+        pathname: '/**', // Allow all paths under Instagram's CDN
+      },
+    ],
   },
-    images: {
-      // Configure remote image patterns for Next.js Image component
-      remotePatterns: [
-        {
-          protocol: 'http',
-          hostname: '127.0.0.1',
-          port: '8000',
-          pathname: '/media/**',
-        },
-        {
-          protocol: 'https',
-          hostname: '*.cdninstagram.com', // Match all Instagram CDN hostnames
-          pathname: '/**', // Allow all paths under Instagram's CDN
-        },
-      ],
-    },
-    async headers() {
-      return [
-        {
-          // Caching static assets in the /images directory under public
-          source: "/images/(.*)", // This matches all files in the /public/images folder
-          headers: [
-            {
-              key: "Cache-Control",
-              value: "public, max-age=31536000, immutable",
-            },
-          ],
-        },
-        {
-          // Handling CORS for API routes
-          source: "/api/(.*)", // Matches all API routes
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "*" }, // Allowing all origins
-            {
-              key: "Access-Control-Allow-Methods",
-              value: "GET, DELETE, PATCH, POST, PUT", // Allowed HTTP methods
-            },
-            {
-              key: "Access-Control-Allow-Headers",
-              value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-            },
-          ],
-        },
-      ];
-    },
-    // Enable React strict mode for highlighting potential problems
-    reactStrictMode: true,
-    sassOptions: {
-      silenceDeprecations: ["legacy-js-api"],
-    }
-  };
+  async headers() {
+    return [
+      {
+        // Caching static assets in the /images directory under public
+        source: "/images/(.*)", // This matches all files in the /public/images folder
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        // Handling CORS for API routes
+        source: "/api/(.*)", // Matches all API routes
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Allowing all origins
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, DELETE, PATCH, POST, PUT", // Allowed HTTP methods
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+  // Enable React strict mode for highlighting potential problems
+  reactStrictMode: true,
+  sassOptions: {
+    silenceDeprecations: ["legacy-js-api"],
+  }
+};
   
-  export default nextConfig;
+export default nextConfig;
