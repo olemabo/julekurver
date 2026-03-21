a006168a59a1d47c3ea76764562fc127bc89b871
Ole Martin Borge <ole.borge@twoday.com>
2026-03-21 17:13:25 +0100

    prøve

--- src/frontend/src/app/page.tsx
+++ src/frontend/src/app/page.tsx
@@ -1,9 +1,6 @@
 import { DEFAULT_LOCALE } from "@/config/i18n";
-import { buildAppRoute } from "@/utils/routes";
 import { redirect } from "next/navigation";
 
 export default function RootPage() {
-  redirect(
-    buildAppRoute({ route: "/[lang]", params: { lang: DEFAULT_LOCALE } }),
-  );
+  redirect(`/${DEFAULT_LOCALE}`);
 }
